973a72ef1459676fbca7719ec9c377765e629e4b
Jack M <melliotfrost@gmail.com>
2015-07-07 17:26:48 -0400

    Adding Cors

--- api/controllers/ApplicationController.js
+++ api/controllers/ApplicationController.js
@@ -24,7 +24,7 @@ module.exports = {
   stations: function(req, res){
     request(['http://api.mygasfeed.com/stations/radius', req.param('lat'), req.param('lng'), req.param('dist'), req.param('type'), req.param('sort'), req.param('apiKey')].join('/') + '.json', function(err, response, body){
       if(err) res.badRequest(err);
-      res.json(body);
+      res.json(JSON.parse(body));
     });
   }
 };
